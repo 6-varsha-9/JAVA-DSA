3d90e67af5aeae3ab22d2c02c1f2c8e6007621d9
Varsha <varshanandakumar069@gmail.com>
2025-03-26 12:04:51 +0530

    PUSH

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Add(23,2);
+    }
+
+    public static void Add(int a , int b){
+        int c = a + b;
+        System.out.println(c);
     }
 }
